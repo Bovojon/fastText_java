9bc185655d8664c98a88fec755106e7206ee5821
Abdulloev <abdullojon.abdulloev@aabdulloev-mbp.angieslist.com>
2018-06-19 11:51:54 -0400

    Change classification-example.sh to run miao's model

--- classification-example.sh
+++ classification-example.sh
@@ -1,44 +1,11 @@
 #!/usr/bin/env bash
-#
-# Copyright (c) 2016-present, Facebook, Inc.
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree. An additional grant
-# of patent rights can be found in the PATENTS file in the same directory.
-#
-
-myshuf() {
-  perl -MList::Util=shuffle -e 'print shuffle(<>);' "$@";
-}
-
-normalize_text() {
-  tr '[:upper:]' '[:lower:]' | sed -e 's/^/__label__/g' | \
-    sed -e "s/'/ ' /g" -e 's/"//g' -e 's/\./ \. /g' -e 's/<br \/>/ /g' \
-        -e 's/,/ , /g' -e 's/(/ ( /g' -e 's/)/ ) /g' -e 's/\!/ \! /g' \
-        -e 's/\?/ \? /g' -e 's/\;/ /g' -e 's/\:/ /g' | tr -s " " | myshuf
-}
-
-RESULTDIR=result
-DATADIR=data
-
-mkdir -p "${RESULTDIR}"
-mkdir -p "${DATADIR}"
-
-if [ ! -f "${DATADIR}/dbpedia.train" ]
-then
-  wget -c "https://github.com/le-scientifique/torchDatasets/raw/master/dbpedia_csv.tar.gz" -O "${DATADIR}/dbpedia_csv.tar.gz"
-  tar -xzvf "${DATADIR}/dbpedia_csv.tar.gz" -C "${DATADIR}"
-  cat "${DATADIR}/dbpedia_csv/train.csv" | normalize_text > "${DATADIR}/dbpedia.train"
-  cat "${DATADIR}/dbpedia_csv/test.csv" | normalize_text > "${DATADIR}/dbpedia.test"
-fi
 
 mvn package
 
 JAR=./target/fasttext-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 
-java -jar ${JAR} supervised -input "${DATADIR}/dbpedia.train" -output "${RESULTDIR}/dbpedia" -dim 10 -lr 0.1 -wordNgrams 2 -minCount 1 -bucket 10000000 -epoch 5 -thread 4
+java -jar ${JAR} supervised -input "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/data/SDrive_Training/autocomplete_lc_forfasttext.txt" -output "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/models/miao_models_testing/miao_self_generate_java_fasttext/miao_ac_ftjavamodel" -dim 100 -lr 0.1 -epoch 50 -loss hs -minCount 5 -wordNgrams 2 -minn 3 -maxn 6 -thread 8
 
-java -jar ${JAR} test "${RESULTDIR}/dbpedia.bin" "${DATADIR}/dbpedia.test"
+java -jar ${JAR} test "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/models/miao_models_testing/miao_self_generate_java_fasttext/miao_ac_ftjavamodel.bin" "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/data/testing/ac_test.txt"
 
-java -jar ${JAR} predict "${RESULTDIR}/dbpedia.bin" "${DATADIR}/dbpedia.test" > "${RESULTDIR}/dbpedia.test.predict"
+java -jar ${JAR} predict "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/models/miao_models_testing/miao_self_generate_java_fasttext/miao_ac_ftjavamodel.bin" "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/data/testing/ac_test.txt" > "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/jon_scripts/miao_predict.txt"
