b539418bcab1fcf797b5d400562c3f059bb62123
Abdulloev <abdullojon.abdulloev@aabdulloev-mbp.angieslist.com>
2018-06-19 16:17:25 -0400

    Change files locations

--- classification-example.sh
+++ classification-example.sh
@@ -4,8 +4,8 @@ mvn package
 
 JAR=./target/fasttext-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 
-java -jar ${JAR} supervised -input "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/data/SDrive_Training/autocomplete_lc_forfasttext.txt" -output "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/models/miao_models_testing/miao_self_generate_java_fasttext/miao_ac_ftjavamodel" -dim 100 -lr 0.1 -epoch 50 -loss hs -minCount 5 -wordNgrams 2 -minn 3 -maxn 6 -thread 8
+java -jar ${JAR} supervised -input "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/data/autocomplete_nov_2015_feb_2018.txt" -output "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/models/jon_models/java/autocomplete_nov_2015_feb_2018" -dim 100 -lr 0.1 -epoch 50 -loss hs -minCount 5 -wordNgrams 2 -minn 3 -maxn 6 -thread 8
 
-java -jar ${JAR} test "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/models/miao_models_testing/miao_self_generate_java_fasttext/miao_ac_ftjavamodel.bin" "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/data/testing/ac_test.txt"
+java -jar ${JAR} test "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/models/jon_models/java/autocomplete_nov_2015_feb_2018.bin" "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/data/testing/ac_test.txt"
 
-java -jar ${JAR} predict "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/models/miao_models_testing/miao_self_generate_java_fasttext/miao_ac_ftjavamodel.bin" "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/data/testing/ac_test.txt" > "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/jon_scripts/miao_predict.txt"
+# java -jar ${JAR} predict "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/models/miao_models_testing/miao_self_generate_java_fasttext/miao_ac_ftjavamodel.bin" "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/data/testing/ac_test.txt" > "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/jon_scripts/miao_predict.txt"
