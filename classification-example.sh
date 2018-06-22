71e953472c58483df0671aee6bbe69d6ee16f470
Abdulloev <abdullojon.abdulloev@aabdulloev-mbp.angieslist.com>
2018-06-22 12:16:14 -0400

    Add preprocessed data in classification-example.sh

--- classification-example.sh
+++ classification-example.sh
@@ -4,8 +4,8 @@ mvn package
 
 JAR=./target/fasttext-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 
-java -jar ${JAR} supervised -input "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/data/autocomplete_nov_2015_feb_2018.txt" -output "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/models/jon_models/java/autocomplete_nov_2015_feb_2018" -dim 100 -lr 0.1 -epoch 50 -loss hs -minCount 5 -wordNgrams 2 -minn 3 -maxn 6 -thread 8
+java -jar ${JAR} supervised -input "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/data/training/autocomplete_nov_2015_feb_2018_preprocessed.txt" -output "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/models/jon_models/test_models/june_21_6" -dim 200 -lr 0.1 -epoch 50 -loss hs -wordNgrams 2 -minn 2 -maxn 6 -thread 8
 
-java -jar ${JAR} test "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/models/jon_models/java/autocomplete_nov_2015_feb_2018.bin" "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/data/testing/ac_test.txt"
+java -jar ${JAR} test "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/models/jon_models/test_models/june_21_6.bin" "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/data/validation/autocomplete_march_april_2018_preprocessed.txt"
 
 # java -jar ${JAR} predict "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/models/miao_models_testing/miao_self_generate_java_fasttext/miao_ac_ftjavamodel.bin" "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/data/testing/ac_test.txt" > "/Users/abdullojon.abdulloev/Desktop/project_autocomplete/jon_scripts/miao_predict.txt"
